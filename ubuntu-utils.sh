e510fb6567c635ab5a942ca0ef49413ed512904d
Scott Chen <rightson@gmail.com>
2017-03-30 19:57:56 +0800

    Support docker and docker-compose installation

--- ubuntu-utils.sh
+++ ubuntu-utils.sh
@@ -80,6 +80,28 @@ Nvidia() {
     sudo apt-get -f install
 }
 
+AptOverHttps() {
+   sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+}
+
+Docker() {
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+	sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+	sudo apt-get update
+	sudo apt-get install -y docker-ce
+}
+
+DockerCompose() {
+    local dst=/usr/local/bin/docker-compose
+    if [ ! -f $dst ]; then
+        sudo curl -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)" -o $dst
+        sudo chmod a+rx $dst
+    else
+        echo "docker-compose installed at $dst"
+    fi
+    $dst --version
+}
+
 Basics() {
     Vim
     Git
