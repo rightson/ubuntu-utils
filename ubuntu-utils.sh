023b6197a6230b23e0c8e0b37280fec60c84b390
Scott Chen <rightson@gmail.com>
2017-04-02 08:06:58 -0700

    oh-my-zsh needs curl

--- ubuntu-utils.sh
+++ ubuntu-utils.sh
@@ -30,7 +30,9 @@ GitCacheTimeout() {
 }
 
 Zsh() {
+    $INSTALL curl
     $INSTALL zsh
+    chsh -s `which zsh`
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 }
 
